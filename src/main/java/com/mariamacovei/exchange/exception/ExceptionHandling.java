2cd3cf0a891b0c188458239c04661ae17a6dd4af
Maria Macovei <macovei.maria1999@gmail.com>
2023-01-25 23:49:13 +0200

    add ExceptionHandling

---
+++ src/main/java/com/mariamacovei/exchange/exception/ExceptionHandling.java
@@ -0,0 +1,45 @@
+package com.mariamacovei.exchange.exception;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static java.time.LocalTime.now;
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@ControllerAdvice
+public class ExceptionHandling {
+
+    @ExceptionHandler(value = {ExchangeRateNotFoundException.class, CashNotFoundException.class, CurrencyCodeNoteFoundException.class, NotFoundAnyEmployees.class})
+    public ResponseEntity<Object> handleExchangeRateNotFoundException(
+            RuntimeException ex) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", now());
+        body.put("message", methodArgumentNotValidException
+                .getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(toList()));
+
+        return new ResponseEntity<>(body, BAD_REQUEST);
+    }
+}
